4d466141d9116b15076bb5cb02a88cbfcc2b6db7
Sam Fernandes <samwallflowergirl@gmail.com>
2022-08-04 21:10:46 -0300

    testing useReducer() hook

--- src/App.js
+++ src/App.js
@@ -5,10 +5,30 @@ import Contact from './component/Contact';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Product from './component/Product';
+import { useReducer } from 'react';
+
+//useReducer example
+function reduce(state, action) {
+  switch (action) {
+    case 'UP':
+      return state + 1;
+
+    case 'DOWN':
+      return state - 1;
+
+    default:
+      break;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reduce, 0);
+
   return (
     <div className="App">
+      <button onClick={() => dispatch('UP')}>+</button>
+      <button onClick={() => dispatch('DOWN')}>-</button>
+      <p>{state}</p>
       <BrowserRouter>
         <Header />
         <div className="content">
